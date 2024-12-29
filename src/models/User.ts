a0bbc80f431feb05f0f1165f7c2a920ca33252d2
Davinci <mgkelly18@gmail.com>
2024-12-28 17:23:34 -0700

    completed models

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,5 +1,56 @@
-import { Schema, Document, model, ObjectId } from 'mongoose';
+import { Schema, Document, model, Types } from 'mongoose';
 
 interface IUser extends Document {
-    
+    username: string;
+    email: string;
+    thoughts: Types.ObjectId[];
+    friends: Types.ObjectId[];
 }
+
+const userSchema = new Schema<IUser>(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/^\S+@\S+\.\S+$/, "Invalid email format"],
+
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought',
+            },
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User',
+            },
+        ],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true,
+          },
+          id: false,
+    }
+);
+
+
+userSchema.virtual('friendCount').get(function (this: IUser) {
+    return this.friends.length;
+});
+
+const User = model('User', userSchema);
+export default User;
+
+
+
